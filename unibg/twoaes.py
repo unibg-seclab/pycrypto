721962c40a30a4b278878201cc689012a3c71ec4
enricobacis <enrico.bacis@gmail.com>
2016-04-08 11:50:35 +0200

    test twoaes with and without NI

--- unibg/twoaes.py
+++ unibg/twoaes.py
@@ -8,38 +8,45 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-KB = 1024
-MB = 1024 * KB
-GB = 1024 * MB
-TB = 1024 * GB
+KiB = 2**10
+MiB = 2**10 * KiB
+GiB = 2**10 * MiB
 
 # bytegenerator = lambda size: urandom(size)
 bytegenerator = lambda size: bytes(bytearray(size))
 
 k1 = '1234567890123456'
 k2 = 'abcdefghijklmnop'
-use_aesni = False
 
-logger.info('Instantiating ciphers ...')
-a1 = AES.new(k1, mode=AES.MODE_ECB, use_aesni=use_aesni)
-a2 = AES.new(k2, mode=AES.MODE_ECB, use_aesni=use_aesni)
-ta = TWOAES.new(k1 + k2, mode=TWOAES.MODE_ECB, use_aesni=use_aesni)
+def test(use_aesni):
 
-logger.info('Generating data ...')
-sizes = np.array([1*KB, 10*KB, 100*KB,
-                  1*MB, 10*MB, 100*MB])
+    logger.info('Instantiating ciphers ...')
+    a1 = AES.new(k1, mode=AES.MODE_ECB, use_aesni=use_aesni)
+    a2 = AES.new(k2, mode=AES.MODE_ECB, use_aesni=use_aesni)
+    ta = TWOAES.new(k1 + k2, mode=TWOAES.MODE_ECB, use_aesni=use_aesni)
 
-logger.info('Running tests ...')
-pattern = '{:^22} | {:^22} | {:^22}'
-print(pattern.format('SIZE', 'TIME AES+AES', 'TIME TWOAES'))
-print(pattern.format('-'*22, '-'*22, '-'*22))
-pattern = '{:<22} | {:<22,.5f} | {:<22,.5f}'
+    logger.info('Generating data ...')
+    sizes = np.array([1*KiB, 10*KiB, 100*KiB,
+                      1*MiB, 10*MiB, 100*MiB])
 
-def test(size):
-    data = a1.encrypt(bytegenerator(size))
-    result = (timeit(lambda: a2.encrypt(a1.decrypt(data)), number=2),
-              timeit(lambda: ta.encrypt(data), number=2))
-    print(pattern.format(size, *result))
-    return result
+    logger.info('Running tests ...')
+    pattern = '{:^22} | {:^22} | {:^22}'
+    print(pattern.format('SIZE', 'TIME AES+AES', 'TIME TWOAES'))
+    print(pattern.format('-'*22, '-'*22, '-'*22))
+    pattern = '{:<22} | {:<22,.5f} | {:<22,.5f}'
 
-times = [test(size) for size in sizes]
+    def single_test(size):
+        data = a1.encrypt(bytegenerator(size))
+        result = (timeit(lambda: a2.encrypt(a1.decrypt(data)), number=2),
+                  timeit(lambda: ta.encrypt(data), number=2))
+        print(pattern.format(size, *result))
+        return result
+
+    times = [single_test(size) for size in sizes]
+
+if __name__ == '__main__':
+    print('\n' + "WITH AESNI".center(72, '='))
+    test(use_aesni=True)
+    print('\n' + "WITHOUT AESNI".center(72, '='))
+    test(use_aesni=False)
+
